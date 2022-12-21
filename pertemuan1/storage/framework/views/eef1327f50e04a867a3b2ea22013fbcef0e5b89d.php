ea12a9286e5a8f0d0000168580b53e37605d9334
IVANDERHANSON <115467536+IVANDERHANSON@users.noreply.github.com>
2022-12-21 17:48:36 +0700

    pertemuan1

---
+++ pertemuan1/storage/framework/views/eef1327f50e04a867a3b2ea22013fbcef0e5b89d.php
@@ -0,0 +1,21 @@
+<?php $__env->startSection('title', 'welcome'); ?>
+
+<?php $__env->startSection('body'); ?>
+<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+
+  
+
+  <?php for($i = 0; $i < 10; $i++): ?>
+    <?php echo e($i); ?>
+
+  <?php endfor; ?>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\hanso\Desktop\TPM\pertemuan1\resources\views/welcome.blade.php ENDPATH**/ ?>
